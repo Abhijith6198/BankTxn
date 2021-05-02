1524cad4a555486b384d56627347e8da2038f73d
Abhijith <abhijithsatyasai@gmail.com>
2021-05-02 21:36:06 +0530

    Adding BankTxn Code

---
+++ src/Abhijith/Account.java
@@ -0,0 +1,40 @@
+package Abhijith;
+
+public class Account
+{
+	String name;
+	int Id;
+	int Balance;
+	int Total=30000;
+	
+// Function for Debit
+	public void Debit(int num1)
+	{
+		Total -= num1;
+		Balance = Total;
+		System.out.println("The updated Balance is:"+Balance);
+	}
+
+// Function for Credit
+	public void Credit(int num2)
+	{
+		Total += num2;
+		Balance = Total;
+		System.out.println("The updated Balance is:"+Balance);
+	}
+	
+// Function to Show Balance	
+	public void showBalance()
+	{
+		System.out.println("The Balance in your account is:"+Total);
+	}
+	
+	public static void main(String[] args) 
+	{
+		Account acc = new Account();
+		acc.Debit(5000);
+		acc.Credit(15000);
+		acc.showBalance();
+	}
+	
+}
